7e41374c83b3033e7f3b4ecbf026de543184ab43
bitfancy <bitbanana717@gmail.com>
2025-02-05 04:17:23 +0900

    update: next auth

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,28 @@
-import NextAuth, {AuthOptions} from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
+// import NextAuth, {AuthOptions} from "next-auth";
+// import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions: AuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            authorization: {
-                params: { prompt: 'select_account' },
-            },
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-        strategy: "jwt",
-    },
-};
+// export const authOptions: AuthOptions = {
+//     providers: [
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID!,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//             authorization: {
+//                 params: { prompt: 'select_account' },
+//             },
+//         }),
+//     ],
+//     secret: process.env.NEXTAUTH_SECRET,
+//     session: {
+//         strategy: "jwt",
+//     },
+// };
+
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST};
+
+// src/app/api/auth/[...nextauth]/route.ts
+import NextAuth from 'next-auth';
+import { authOptions } from "@/lib/auth/authUtils"
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST};
+export { handler as GET, handler as POST };
